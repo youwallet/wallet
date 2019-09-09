8dae495f13976ad76bd92d9fbf858e8cdf681755
赵冰龙 <binglong@from.mac>
2019-09-09 11:41:43 +0800

    for #12 调用指纹识别

--- flutter_app/lib/debug_page.dart
+++ flutter_app/lib/debug_page.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart'; // 官方组件库
-//import 'package:image_picker_saver/image_picker_saver.dart' as ImagePickerSaver;
-import 'package:image_picker_saver/image_picker_saver.dart';
-import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart'; // 官方组件库
+//import 'package:image_picker_saver/image_picker_saver.dart';
+import 'package:http/http.dart' as http;
+//import 'package:image_picker/image_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:local_auth/local_auth.dart';
 
 class DebugPage extends StatefulWidget {
   DebugPage() : super();
@@ -11,7 +14,11 @@ class DebugPage extends StatefulWidget {
 }
 
 class _DebugPageState extends State<DebugPage> {
+  /// 本地认证框架
+  final LocalAuthentication auth = LocalAuthentication();
   File _imageFile;
+  bool _canCheckBiometrics; // 是否有可用的生物识别技术
+  String _authorized = '验证失败';
   // 保存图片到相册
   void _onImageSaveButtonPressed() async {
     print("_onImageSaveButtonPressed");
@@ -20,15 +27,25 @@ class _DebugPageState extends State<DebugPage> {
 
     debugPrint(response.statusCode.toString());
 
-    var filePath = await ImagePickerSaver.saveFile(
-        fileData: response.bodyBytes);
-
-    var savedFile= File.fromUri(Uri.file(filePath));
-    debugPrint(filePath);
+//    var filePath = await ImagePickerSaver.saveFile(
+//        fileData: response.bodyBytes);
+//
+//    var savedFile= File.fromUri(Uri.file(filePath));
+//    debugPrint(filePath);
     setState(() {
       //_imageFile = Future<File>.sync(() => savedFile);
     });
   }
+
+  // 打开相册选取图片
+//  Future getImage() async {
+//    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+//
+//    setState(() {
+//      _imageFile = image;
+//    });
+//  }
+
 
   void _incrementCounter() {
     print('++');
@@ -39,6 +56,40 @@ class _DebugPageState extends State<DebugPage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       //_counter++;
+    });
+  }
+
+  /// 检查是否有可用的生物识别技术
+  Future<Null> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+      print(canCheckBiometrics);
+      _authenticate();
+    } catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+
+  // 生物识别
+  Future<Null> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticateWithBiometrics(
+          localizedReason: '扫描指纹进行身份验证',
+          useErrorDialogs: true,
+          stickyAuth: false);
+    } catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _authorized = authenticated ? '验证通过' : '验证失败';
     });
   }
 
@@ -70,6 +121,19 @@ class _DebugPageState extends State<DebugPage> {
                 print("ok");
                 _onImageSaveButtonPressed();
               },
+            ),
+            FlatButton(
+              child: Text("打开摄像头"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                _onImageSaveButtonPressed();
+              },
+            ),
+            FlatButton(
+              child: Text("指纹识别"),
+              textColor: Colors.blue,
+              onPressed: () {_checkBiometrics();},
             ),
           ],
         ),
