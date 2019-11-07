f7358d872efdf3e49162059529d4992cc6363518
赵冰龙 <binglong@from.mac>
2019-11-07 16:19:25 +0800

    for #24  完善收款样式

--- flutter_app/lib/tab_receive.dart
+++ flutter_app/lib/tab_receive.dart
@@ -21,63 +21,93 @@ class Page extends State<TabReceive> {
     return new Scaffold(
       appBar: buildAppBar(context),
       body: new Center(
-        child: new Container(
-          width: 300.0,
-          height: 200.0,
-
-          decoration: new BoxDecoration(
-            borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
-            color: Colors.lightBlue,
-          ),
-          child: new Column(
-            children: <Widget>[
-              new Text(
-                  'ETH-Wallet',
-                  style: new TextStyle(
-                      fontSize: 24.0,
-                      color: Colors.white
-                  )
+        child: new Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            new Container(
+              padding: const EdgeInsets.only(top: 40.0),
+              width: 300.0,
+              height: 325.0,
+              decoration: new BoxDecoration(
+                borderRadius: new BorderRadius.all(new Radius.circular(8.0)),
+                color: Colors.lightBlue,
               ),
-              new Text(
-                  '0x0000000000000000000000',
-                  style: new TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.white
-                  )
-              ),
-              new Container(
-                padding: EdgeInsets.all(2.0),
-                constraints: BoxConstraints(
-                  minWidth: 100,
-                ),
-                decoration: new BoxDecoration(
-                  borderRadius: new BorderRadius.all(new Radius.circular(4.0)),
-                  color: Color.fromRGBO(0, 0, 0, .3),//设置颜色,.3代表透明度，也可以写成0.3
-                ),
+              child: new Column(
+                children: <Widget>[
+                  new Text(
+                      'ETH-Wallet',
+                      style: new TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.white
+                      )
+                  ),
+                  new Text(
+                      '0x0000000000000000000000',
+                      style: new TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.white
+                      )
+                  ),
+                  new Container(
+                    padding: EdgeInsets.all(2.0),
+                    margin: const EdgeInsets.all(10.0),
+                    constraints: BoxConstraints(
+                      minWidth: 100,
+                    ),
+                    decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.all(new Radius.circular(4.0)),
+                      color: Color.fromRGBO(0, 0, 0, .3),//设置颜色,.3代表透明度，也可以写成0.3
+                    ),
 
-                child: GestureDetector(
-                  child: Text(
-                    '一键复制地址',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0
+                    child: GestureDetector(
+                      child: Text(
+                        '一键复制地址',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0
+                        ),
+                      ),
+                      onTap: _copyAddress,
                     ),
                   ),
-                  onTap: _copyAddress,
-                ),
-              ),
-              new Container(
-                  alignment: Alignment.center,
-                  color: Color(0xFFFFFFFF),
-                  child: new Image.asset(
-                    'images/new_wallet.png', // 图片，已经在pubspec.yaml中定义
-                    height: 100.0,
-                    fit: BoxFit.cover,
-                  ),
+                  new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(bottom: 20.0), // 四周填充边距32像素
 
+
+                      color: Color(0xFFFFFFFF),
+                      child: new Column(
+                        children: <Widget>[
+                          new Padding(
+                            padding: new EdgeInsets.all(10.0),
+                            child: new Text(
+                              '收款地址',
+                              style: TextStyle(
+                                  fontSize: 16.0,
+                                  height: 2
+                              ),
+                            ),
+                          ),
+                          new Image.asset(
+                            'images/new_wallet.png',
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      )
+
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            new Positioned(
+              child:  new Image.asset('assets/images/logo.png'),
+              left: 125,
+              top: -25,
+              width: 50,
+              height: 50,
+            ),
+          ]
         )
       ),
     );
