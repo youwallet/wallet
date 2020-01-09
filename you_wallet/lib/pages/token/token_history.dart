45adb7e90809e1f1f30f50379802e4c771d00b1b
赵冰龙 <binglong@from.mac>
2020-01-09 17:53:00 +0800

    for #49 页面显示转账记录

--- you_wallet/lib/pages/token/token_history.dart
+++ you_wallet/lib/pages/token/token_history.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:web3dart/json_rpc.dart';
 import 'package:youwallet/widgets/menu.dart';
+import 'package:youwallet/db/sql_util.dart';
 
 class TokenHistory extends StatefulWidget {
   @override
@@ -13,98 +14,98 @@ class TokenHistory extends StatefulWidget {
 
 class Page extends State<TokenHistory> {
 
+  List list = [];
+
   @override
   Widget build(BuildContext context) {
     return layout(context);
   }
 
-  Widget layout(BuildContext context) {
-    return new Scaffold(
-        appBar: buildAppBar(context),
-        body: new Column(
-          children: <Widget>[
-            _buildBar(),
-            new Container(
-                padding: new EdgeInsets.all(16.0),
-                child: new Column(
-                  children: <Widget>[
-                    new Row(
-                      children: <Widget>[
-                        new Padding(
-                          padding: new EdgeInsets.only(right: 20.0),
-                          child: new Text('收款转账'),
-                        ),
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    this._getHistory();
+  }
 
-                        new Text('历史兑换'),
-                      ],
+  Widget layout(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: new Scaffold(
+        appBar: buildAppBar(context),
+        body: new TabBarView(
+          children: [
+            _getHistoryToken(),
+            _getHistoryToken(),
+            _getHistoryToken(),
+            _getHistoryToken(),
+            _getHistoryToken()
+          ],
+        ),
+        bottomNavigationBar: new BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: new SizedBox(
+                  height: 50.0,
+                  child: RaisedButton.icon(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0))
                     ),
-                    _getHistoryToken(),
-                  ],
-                )
-            ),
-          ],
+                    elevation: 0,
+                    icon: Icon(Icons.search),
+                    label: Text("兑换"),
+                    onPressed: (){
+                      Navigator.pushNamed(context, "tab_exchange");
+                    },
+                  ),
+                ),
+                flex: 1,
+              ),
+              Expanded(
+                child: new SizedBox(
+                  height: 50.0,
+                  child: RaisedButton.icon(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0))
+                    ),
+                    elevation: 0.0,
+                    icon: Icon(Icons.search),
+                    color: Colors.blue,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.blue,
+                    label: Text("收款"),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "tab_transfer");
+                    },
+                  ),
+                ),
+                flex: 1,
+              ),
+              Expanded(
+                child: new SizedBox(
+                  height: 50.0,
+                  child: RaisedButton.icon(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0))
+                    ),
+                    elevation: 0,
+                    icon: Icon(Icons.search),
+                    label: Text("转账"),
+                    color: Colors.green,
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                flex: 1,
+              ),
+            ],
+          ),
         ),
-      bottomNavigationBar: new BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: new SizedBox(
-                height: 50.0,
-                child: RaisedButton.icon(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(0))
-                  ),
-                  elevation: 0,
-                  icon: Icon(Icons.search),
-                  label: Text("兑换"),
-                  onPressed: (){
-                    Navigator.pushNamed(context, "tab_exchange");
-                  },
-                ),
-              ),
-              flex: 1,
-            ),
-            Expanded(
-              child: new SizedBox(
-                height: 50.0,
-                child: RaisedButton.icon(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(0))
-                  ),
-                  elevation: 0,
-                  icon: Icon(Icons.search),
-                  color: Colors.blue,
-                  label: Text("收款"),
-                  onPressed: () {
-                    Navigator.pushNamed(context, "tab_transfer");
-                  },
-                ),
-              ),
-              flex: 1,
-            ),
-            Expanded(
-              child: new SizedBox(
-                height: 50.0,
-                child: RaisedButton.icon(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(0))
-                  ),
-                  elevation: 0,
-                  icon: Icon(Icons.search),
-                  label: Text("转账"),
-                  color: Colors.green,
-                  onPressed: (){
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ),
-              flex: 1,
-            ),
-          ],
-        ),
-      ),
-      );
+      )
+    );
+
   }
 
   // 构建app bar
@@ -112,13 +113,15 @@ class Page extends State<TokenHistory> {
     return new AppBar(
         backgroundColor: Colors.white,
         title: new Text('转账记录'),
-//        bottom: new TabBar(
-//          tabs: [
-//            new Tab(text: '日期'),
-//            new Tab(text: '今天'),
-//            new Tab(text: '三天')
-//          ],
-//        )
+        bottom: new TabBar(
+          tabs: [
+            new Tab(text: '日期'),
+            new Tab(text: '今天'),
+            new Tab(text: '三天'),
+            new Tab(text: '本周'),
+            new Tab(text: '本月')
+          ],
+        )
       //leading: new Icon(Icons.account_balance_wallet),
     );
   }
@@ -156,16 +159,14 @@ class Page extends State<TokenHistory> {
 
   _getHistoryToken() {
     return new Container(
+      padding: new EdgeInsets.all(16.0),
       child: new Column(
-        children: <Widget>[
-          _buildToken(),
-          _buildToken()
-        ],
+        children: this.list.map((item) => _buildToken(item)).toList()
       ),
     );
   }
 
-  _buildToken() {
+  Widget _buildToken(item) {
     return new Container(
         padding: const EdgeInsets.only(top: 12.0,bottom: 12.0), // 四周填充边距32像素
         decoration: new BoxDecoration(
@@ -176,7 +177,7 @@ class Page extends State<TokenHistory> {
             new Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                new Text('收款-fromtoken'),
+                new Text('收款-${item['fromAddress']}'),
                 new Text(
                     '+0.123 token',
                     style: new TextStyle(
@@ -210,6 +211,15 @@ class Page extends State<TokenHistory> {
   @override
   _onClick() {
     debugPrint("onTap");
+  }
+
+  void _getHistory() async {
+    var sql = SqlUtil.setTable("transfer");
+    List arr = await sql.get();
+    print(arr);
+    setState(() {
+      this.list = arr;
+    });
   }
 
 //  void _onTabChanged() {
