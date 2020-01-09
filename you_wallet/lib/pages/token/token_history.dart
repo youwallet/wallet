dbda62353aafca2fc3782b520b9da4287d1eba79
赵冰龙 <binglong@from.mac>
2020-01-09 23:17:37 +0800

    for #49 增加三个字体图标

--- you_wallet/lib/pages/token/token_history.dart
+++ you_wallet/lib/pages/token/token_history.dart
@@ -4,6 +4,8 @@ import 'package:http/http.dart';
 import 'package:web3dart/json_rpc.dart';
 import 'package:youwallet/widgets/menu.dart';
 import 'package:youwallet/db/sql_util.dart';
+import 'package:common_utils/common_utils.dart';
+import 'package:youwallet/bus.dart';
 
 class TokenHistory extends StatefulWidget {
   @override
@@ -53,10 +55,13 @@ class Page extends State<TokenHistory> {
                         borderRadius: BorderRadius.all(Radius.circular(0))
                     ),
                     elevation: 0,
-                    icon: Icon(Icons.search),
-                    label: Text("兑换"),
+                    icon: Icon(IconData(0xe6eb, fontFamily: 'iconfont')),
+                    label: Text(
+                        "兑换",
+                    ),
                     onPressed: (){
-                      Navigator.pushNamed(context, "tab_exchange");
+                      eventBus.fire(TabChangeEvent(1));
+                      Navigator.of(context).pop();
                     },
                   ),
                 ),
@@ -70,13 +75,19 @@ class Page extends State<TokenHistory> {
                         borderRadius: BorderRadius.all(Radius.circular(0))
                     ),
                     elevation: 0.0,
-                    icon: Icon(Icons.search),
+                    icon: Icon(IconData(0xe624, fontFamily: 'iconfont'),color: Colors.white),
                     color: Colors.blue,
                     highlightColor: Colors.blue,
                     splashColor: Colors.blue,
-                    label: Text("收款"),
+                    label: Text(
+                        "收款",
+                        style: new TextStyle(
+                            color: Colors.white
+                        )
+                    ),
                     onPressed: () {
-                      Navigator.pushNamed(context, "tab_transfer");
+                      eventBus.fire(TabChangeEvent(2));
+                      Navigator.of(context).pop();
                     },
                   ),
                 ),
@@ -90,8 +101,13 @@ class Page extends State<TokenHistory> {
                         borderRadius: BorderRadius.all(Radius.circular(0))
                     ),
                     elevation: 0,
-                    icon: Icon(Icons.search),
-                    label: Text("转账"),
+                    icon: Icon(IconData(0xe616, fontFamily: 'iconfont'),color: Colors.white),
+                    label: Text(
+                        "转账",
+                        style: new TextStyle(
+                            color: Colors.white
+                        )
+                    ),
                     color: Colors.green,
                     onPressed: (){
                       Navigator.of(context).pop();
@@ -167,6 +183,7 @@ class Page extends State<TokenHistory> {
   }
 
   Widget _buildToken(item) {
+    String date = DateUtil.formatDateMs( int.parse( item['createTime']), format: DataFormats.full);
     return new Container(
         padding: const EdgeInsets.only(top: 12.0,bottom: 12.0), // 四周填充边距32像素
         decoration: new BoxDecoration(
@@ -177,9 +194,9 @@ class Page extends State<TokenHistory> {
             new Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                new Text('收款-${item['fromAddress']}'),
+                new Text('收款-${item['tokenName']}'),
                 new Text(
-                    '+0.123 token',
+                    '-${item['num']} token',
                     style: new TextStyle(
                         color: Colors.lightBlue
                     )
@@ -190,13 +207,13 @@ class Page extends State<TokenHistory> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 new Text(
-                    '2019-11-11 10:11:12',
+                    '${date}',
                     style: new TextStyle(
                         color: Colors.black38
                     )
                 ),
                 new Text(
-                    '转账中',
+                    '${item['status']??'转账中'}',
                     style: new TextStyle(
                         color: Colors.deepOrange
                     )
