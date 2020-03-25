5c86d1a11037aa060c3252f5e6169ada5046ea4e
赵冰龙 <570869214@qq.com>
2020-03-25 16:49:49 +0800

    for #94 深度交易列表显示

--- you_wallet/lib/pages/order/order_deep.dart
+++ you_wallet/lib/pages/order/order_deep.dart
@@ -7,7 +7,7 @@ import 'package:youwallet/db/sql_util.dart';
 import 'package:common_utils/common_utils.dart';
 import 'package:youwallet/bus.dart';
 import 'package:youwallet/service/trade.dart';
-
+import 'dart:math';
 
 class OrderDeep extends StatefulWidget {
 
@@ -48,9 +48,14 @@ class Page extends State<OrderDeep> {
         length: 5,
         child: new Scaffold(
           appBar: buildAppBar(context),
-          body: Column(
-              children: this.deep.map((item) => buildItem(item, context)).toList(),
+          body: new ListView(
+            children: <Widget>[
+              Column(
+                children: this.deep.map((item) => buildItem(item, context)).toList(),
+              )
+            ],
           )
+
         )
     );
 
@@ -69,20 +74,57 @@ class Page extends State<OrderDeep> {
   // 注意这里token的顺序，BTA-BTC和BTC-BTA的bq hash是不一样的
   // 同一个队列，卖单和买单的bq hash是一样的
   void getOrderDeep() async {
-    print(widget.arguments);
-    print(widget.arguments['leftToken']);
     String leftToken = widget.arguments['leftToken']['address'];
     String rightToken = widget.arguments['rightToken']['address'];
     // 获取卖单的bq hash
     String hash = await Trade.getOrderQueueInfo(leftToken, rightToken, true);
     String bqHash = hash.replaceFirst('0x', '').substring(0,64);
-    print(bqHash);
+
     String res = await Trade.getOrderDepth(bqHash);
-    print(res);
+    String data = res.replaceFirst('0x', '');
+    int len = data.length;
+    int index = (len/192).toInt();
+    int i = 0;
+    List arr = [];
+    while( i<index) {
+      String item = data.substring(i*192, i*192 + 192);
+      double amount = BigInt.parse(item.substring(0, 64), radix: 16)/BigInt.from(pow(10, 18));
+      double price = BigInt.parse(item.substring(64, 128), radix: 16)/BigInt.from(pow(10, 18));
+      double filled = BigInt.parse(item.substring(128, 192), radix: 16)/BigInt.from(pow(10, 18));
+      arr.add({
+        'price': price.toString(),
+        'amount': amount.toString(),
+        'filled': filled.toString(),
+      });
+      i = i+1;
+    }
+
+    this.setState((){
+      this.deep = arr;
+    });
   }
 
   Widget buildItem(Map item, context) {
-    return Text('ok');
+    return new Container(
+      color: Colors.black12, //16进制颜色
+      padding: const EdgeInsets.all(4.0),
+      margin: const EdgeInsets.only(top: 10.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          new Text(
+              item['price'],
+              style: TextStyle(color: Colors.green)
+          ),
+          new Icon(
+              Icons.close,
+              size: 20.0,
+              color:  Colors.green
+          ),
+          new Text(item['amount']),
+        ],
+      ),
+    );
   }
 
 
