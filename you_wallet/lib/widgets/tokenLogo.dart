7388b44ee87b525bce7ed0113d652e95672f4fad
赵冰龙 <570869214@qq.com>
2020-05-22 12:21:20 +0800

    fix #113 用stack解决容器之间有缝隙的问题

--- you_wallet/lib/widgets/tokenLogo.dart
+++ you_wallet/lib/widgets/tokenLogo.dart
@@ -15,17 +15,39 @@ class TokenLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     Map token = Global.hotToken.firstWhere((element)=>(element['address'] == this.address),orElse: ()=>({}));
     if (token.isEmpty) {
-      return CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 25.0,
-          child: Icon(IconData(0xe648, fontFamily: 'iconfont'),size: 50.0, color: Colors.black26)
+      return Stack(
+        children: <Widget>[
+          Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: new BoxDecoration(
+                // border: new Border.all(width: 2.4, color: Colors.black26),
+                // borderRadius: new BorderRadius.all(new Radius.circular(25.0))
+            ),
+          ),
+          Positioned(
+              child: Icon(IconData(0xe648, fontFamily: 'iconfont'),size: 50.0, color: Colors.black26)
+          ),
+        ],
       );
     } else {
-      return CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 25.0,
-          child: Icon(IconData(token['icon'], fontFamily: 'iconfont'),size: 50.0, color: token['color'])
+      return Stack(
+        children: <Widget>[
+          Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: new BoxDecoration(
+                border: new Border.all(width: 2.4, color: token['color']),
+                color: Colors.white,
+                borderRadius: new BorderRadius.all(new Radius.circular(25.0))
+            ),
+          ),
+          Positioned(
+            child:Icon(IconData(token['icon'], fontFamily: 'iconfont'),size: 50.0, color: token['color'])
+          ),
+        ],
       );
+
     }
   }
 }
